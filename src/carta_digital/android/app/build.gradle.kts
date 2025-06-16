6c15a888d9e62722b791fe627055f8a18014de94
Fedezu00 <federico.zulaica@mi.unc.edu.ar>
2025-06-16 19:08:21 -0300

    FirmaYkeytool2

--- src/carta_digital/android/app/build.gradle.kts
+++ src/carta_digital/android/app/build.gradle.kts
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.carta_digital"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,18 +28,18 @@ android {
     }
 
     signingConfigs {
-    release {
-        storeFile file("release-key.jks")
-        storePassword "cartadigital2025"
-        keyAlias "release-key"
-        keyPassword "cartadigital2025"
+        create("release") {
+            storeFile = file("release-key.jks")
+            storePassword = System.getenv("RELEASE_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("RELEASE_KEY_ALIAS")
+            keyPassword = System.getenv("RELEASE_KEY_PASSWORD")
+        }
     }
-}
 
     buildTypes {
-    release {
-        signingConfig signingConfigs.release
-    }
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
     }
 }
 
